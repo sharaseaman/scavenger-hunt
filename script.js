991538484a84ea8cbfe3abbae0f8b98cd91b4821
sharaseaman <shara.seaman@gmail.com>
2017-08-29 22:41:54 -0500

    update this

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('testing');
+
+function reverse(str) {
+    return str.split("").reverse().join("");
+  }
+  
+  var myString = "This is a string";
+  
+  console.log(reverse(myString));
+  
